ef568c478ed0f697033e9aa603e11b276480dce3
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-05-03 13:19:15 +0100

    Delete nano.sh

--- nano.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh 
-printf "\033c\033[43;30m\ndownloa gcc"
-wget "https://www.nano-editor.org/dist/v4/nano-4.0.tar.gz" 
-gzip -d 'nano-4.0.tar.gz'
-tar --extract -f 'nano-4.0.tar'
-cd linux-6.15-rc1
- 
